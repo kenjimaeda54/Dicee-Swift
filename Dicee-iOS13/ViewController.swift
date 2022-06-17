f10508c77b701a4c7944891c2c5c20c8111a634c
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-06-17 09:07:49 -0300

    feat:finished project

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,13 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageViewOne: UIImageView!
+    @IBOutlet weak var imageViewTwo: UIImageView!
+    let imageOne = UIImage(named: "DiceOne")
+    let imageTwo = UIImage(named: "DiceTwo")
+    let imageThree = UIImage(named: "DiceThree")
+    let imageFour = UIImage(named: "DiceFour")
+    let imageFive = UIImage(named: "DiceFive")
+    let imageSix = UIImage(named: "DiceSix")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        // Do any additional setup after loading the view.
+        
+        
     }
 
+    @IBAction func changeView(_ sender: UIButton) {
+        let arrayImage = [imageOne,imageTwo,imageThree,imageFour,imageFive,imageSix]
 
+        
+        //imageViewTwo.image = arrayImage[Int.random(in: 0...5)] uma maneira de fazer aleatorio
+        imageViewTwo.image = arrayImage.randomElement() as? UIImage
+        imageViewOne.image = arrayImage.randomElement() as? UIImage
+  
+    }
+    
 }
 
